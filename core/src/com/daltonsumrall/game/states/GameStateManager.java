4e6f0ff111e4f064b5d4fc3c6436f94f74a188cc
Dalton Sumrall <daltonsumrall@gmail.com>
2016-10-02 01:23:05 -0500

    Part 3: Put stack functions in GameStateManager class. Created MenuState class.

---
+++ core/src/com/daltonsumrall/game/states/GameStateManager.java
@@ -0,0 +1,39 @@
+package com.daltonsumrall.game.states;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.Stack;
+
+/**
+ * Created by dalton on 10/2/16.
+ */
+
+public class GameStateManager {
+    private Stack<State> states;
+
+    public GameStateManager(){
+        states = new Stack<State>();
+    }
+
+    public void push(State state){
+        states.push(state);
+    }
+
+    public void pop(State state){
+        states.pop();
+    }
+
+    public void set(State state){
+        states.pop();
+        states.push(state);
+    }
+
+    public void update(float deltaTime){
+        states.peek().update(deltaTime);
+    }
+
+    public void render(SpriteBatch spriteBatch){
+        states.peek().render(spriteBatch);
+    }
+
+}
---
+++ core/src/com/daltonsumrall/game/states/MenuState.java
@@ -0,0 +1,29 @@
+package com.daltonsumrall.game.states;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+
+/**
+ * Created by dalton on 10/2/16.
+ */
+
+public class MenuState extends State {
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void render(SpriteBatch spriteBatch) {
+
+    }
+
+    @Override
+    public void update(float deltaTime) {
+
+    }
+}
